9408dc6791c1b2923ddd5453cd8001a167e9e6b3
nabutech <nabutech@proton.me>
2025-05-23 11:11:44 +1200

    Created BookService.cs

---
+++ App/App/Services/BookService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Threading.Tasks;
+using app.Models;
+using app.Repositories.Interfaces;
+
+namespace app.Services
+
+{
+    public class BookService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public Task<List<Book>> GetAllBooksAsync() => _bookRepository.GetAllAsync();
+
+        public Task<Book?> GetBookByIdAsync(Guid id) => _bookRepository.GetByIdAsync(id);
+
+        public async Task AddBookAsync(Book book)
+        {
+            await _bookRepository.AddAsync(book);
+            await _bookRepository.SaveChangesAsync();
+        }
+
+        public async Task UpdateBookAsync(Book Book)
+        {
+            await _bookRepository.UpdateAsync(Book);
+            await _bookRepository.SaveChangesAsync();
+        }
+
+        public async Task DeleteBookAsync(Guid Id)
+        {
+            await _bookRepository.DeleteAsync(Id);
+            await _bookRepository.SaveChangesAsync();
+        }
+    }
+}
