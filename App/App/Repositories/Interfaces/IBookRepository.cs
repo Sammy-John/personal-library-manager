55ed8ca297f27711aa990b6d7180295d3a7cd4d2
nabutech <nabutech@proton.me>
2025-05-23 09:49:04 +1200

    Created INoteRepository, IBookRepository

---
+++ App/App/Repositories/Interfaces/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using app.Models;
+
+namespace app.Repositories.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<List<Book>> GetAllSync();
+        Task<List<Book>> GetAllAsync();
+        Task AddAsync (Book book);
+        Task UpdateAsync (Book book);
+        Task DeleteAsync (Guid id);
+        Task SaveChangesAsync();
+    
+    }
+}
---
+++ App/App/Repositories/Interfaces/INoteRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using app.Models;
+
+namespace app.Repositories.Interfaces
+{
+    public interface INoteRepository
+    {
+        Task<List<Note>> GetNotesByBookIdAsync (Guid bookId);
+        Task AddNoteAsync(Note note);
+        Task UpdateNoteAsync(Note note);
+        Task DeleteNoteAsync(Guid noteId);
+        Task SaveChangesAsync();
+
+    }
+}
